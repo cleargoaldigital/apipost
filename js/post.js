5b0da9346639ae6f26ac93983dad4bf5189aa1eb
Taofeek Ajibade <81028891+cleargoaldigital@users.noreply.github.com>
2021-11-01 14:22:51 +0100

    Update button

--- js/post.js
+++ js/post.js
@@ -1,32 +1,30 @@
+function getPosts() {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data)
+    let postLayout = document.getElementById('post-layout')
+    let html = '';
+    data.forEach(e => {
+      // console.log(element)
+      html += `
+      <div class="col-md-4 mb-3">
+      <div class="card h-100">
+      <div class="card-body">
+      <h6 class="post-title mb-4 text-center text-info">${e.id}</h6>
+      <h5 class="post-title mb-4" id="">${e.title}</h5>
+      <p class="post-body">${e.body}</p>
+      </div>
+      <div class="d-flex justify-content-between mx-2 mb-2">
+      <button class="btn btn-primary" onclick="deletePost( ${e.id} )">Delete</button>
 
-
-function getPosts(){
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then((data) => {
-        console.log(data)
-        let postLayout = document.getElementById('post-layout')
-        let html = '';
-        data.forEach(e => {
-            // console.log(element)
-            html += `
-            <div class="col-md-4 mb-3">
-            <div class="card h-100">
-                <div class="card-body">
-                    <h6 class="post-title mb-4 text-center text-info">${e.id}</h6>
-                    <h5 class="post-title mb-4" id="">${e.title}</h5>
-                    <p class="post-body">${e.body}</p>
-                </div>
-                <div class="d-flex justify-content-around mb-2">
-                    <button class="btn btn-primary" onclick="deletePost( ${e.id} )">Delete</button>
-            
-                    <button class="btn btn-success" onclick="updatePost(e)">Update</button>
-                </div>
-            </div>
-            </div>`
-        postLayout.innerHTML =html;
-        });
-    })
+      <button class="btn btn-success" onclick="updatePost(e)">Update</button>
+      </div>
+      </div>
+      </div>`
+      postLayout.innerHTML = html;
+    });
+  })
 }
 getPosts();
 
@@ -37,31 +35,31 @@ let postForm = document.querySelector('#post-form')
 
 postForm.addEventListener('submit', createPost)
 
- function createPost(e) {
-    e.preventDefault();
-    let pTitle = postTitle.value;
-    let pBody = postBody.value;
-    console.log('Post Title', pTitle)
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-            title: pTitle,
-            body: pBody,
-            userId: 5
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        console.log('post', data);
-        console.log(userPost)
-        userPost.push(data);
-        console.log(userPost)
+function createPost(e) {
+  e.preventDefault();
+  let pTitle = postTitle.value;
+  let pBody = postBody.value;
+  console.log('Post Title', pTitle)
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: pTitle,
+      body: pBody,
+      userId: 5
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    console.log('post', data);
+    console.log(userPost)
+    userPost.push(data);
+    console.log(userPost)
     alert('Congratulations! You have successfully created your post.')
-        
-    })
+
+  })
 }
 
 
@@ -70,30 +68,29 @@ postForm.addEventListener('submit', createPost)
 The function below is written to allow deleting the data. */
 
 function deletePost(postId) {
-    console.log(postId)
+  console.log(postId)
 
 }
 /* Update function
 The function below is written to allow the updating of the data. */
 
 function updatePost() {
-    fetch('https://jsonplaceholder.typicode.com/posts/1', {
-        method: 'PUT',
-        body: JSON.stringify({
-            id: 1,
-            title: 'foo',
-            body: 'bar',
-            userId: 1,
+  fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    method: 'PUT',
+    body: JSON.stringify({
+      id: 1,
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
     }),
     headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+      'Content-type': 'application/json; charset=UTF-8',
     },
-})
-    .then((response) => response.json())
-    .then((data) => { 
-        console.log(data)
-})
-.catch((e) => {
-})
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+  })
+  .catch((e) => {})
 
 }
